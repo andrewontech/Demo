ca511d0a6370290b1ca90a498c978bcede1aab9c
Andrew Hilvers <drewhilvers@gmail.com>
2017-04-18 14:57:55 -0700

    Create and Use: Unity Button

---
+++ UnityProject/Assets/Code/ButtonTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour {
+  //Create serializable field for our button and our cube
+  public Button MyButton = null;
+  public GameObject TargetCube = null;
+
+  private void Start() {
+    //Subscribe to the onclick of MyButton
+    MyButton.onClick.AddListener(OnMyButtonClick);
+  }
+
+  private void OnMyButtonClick() {
+    //Turn off TargetCube
+    TargetCube.SetActive(false);
+  }
+}
